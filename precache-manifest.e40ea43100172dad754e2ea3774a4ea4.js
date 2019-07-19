e8938e35158b5c354e656bd8679112b1273fff9d
veronica <v.blazhko@beauit.com>
2019-07-19 12:22:22 +0300

    Updates

---
+++ precache-manifest.e40ea43100172dad754e2ea3774a4ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-promo/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "1730974aeac13e7afb75",
+    "url": "/vk-promo/static/js/runtime~main.1730974a.js"
+  },
+  {
+    "revision": "d621444bc69f546bb997",
+    "url": "/vk-promo/static/js/main.d621444b.chunk.js"
+  },
+  {
+    "revision": "80a69e8f511fedab44b3",
+    "url": "/vk-promo/static/js/2.80a69e8f.chunk.js"
+  },
+  {
+    "revision": "d621444bc69f546bb997",
+    "url": "/vk-promo/static/css/main.c0956da8.chunk.css"
+  },
+  {
+    "revision": "80a69e8f511fedab44b3",
+    "url": "/vk-promo/static/css/2.4c048e32.chunk.css"
+  },
+  {
+    "revision": "26b27f3c9562f2733d8fd59891b1f6af",
+    "url": "/vk-promo/index.html"
+  }
+];
